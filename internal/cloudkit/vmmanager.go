46a8e1f61ee4cd65006c98b073374018b2c30032
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-11-10 20:36:04 -0700

    internal/{cloudkit,server}: work towards spinning up and fetching VMs

--- internal/cloudkit/vmmanager.go
+++ internal/cloudkit/vmmanager.go
@@ -3,21 +3,38 @@ package cloudkit
 import (
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/digitalocean/go-libvirt"
+	"github.com/lithammer/shortuuid"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
+
 	libvirtxml "libvirt.org/libvirt-go-xml"
 )
 
-// VM ...
-type VM struct{}
+// VM represents a VM as understood by cloudkit
+type VM struct {
+	ID         int                         `json:"id,omitempty"`
+	Name       string                      `json:"name,omitempty"`
+	State      string                      `json:"state"`
+	IP         string                      `json:"ip,omitempty"`
+	MAC        string                      `json:"mac,omitempty"`
+	Mem        int                         `json:"mem,omitempty"`
+	CurrentMem int                         `json:"current_mem,omitempty"`
+	VCPUs      int                         `json:"vcpus,omitempty"`
+	Type       libvirtxml.DomainOSType     `json:"type,omitempty"`
+	Devices    libvirtxml.DomainDeviceList `json:"devices,omitempty"`
+}
 
 // VMController describes all the actions you can take on a VM.
 type VMController interface {
-	CreateVM() error
-	GetVMs() ([]libvirt.Domain, error)
+	CreateVM(machineType string, memoryInGB int, vCPUs int) error
+	GetRunningVMs() ([]VM, error)
 }
 
 // VMManager imlements the VMController interface and handles
@@ -49,17 +66,106 @@ func NewVMManager(hostLibvirtConnStr string, log *logrus.Logger) (*VMManager, er
 	return &VMManager{libvirt: l, logger: log}, nil
 }
 
-// CreateVM ...
-func (v *VMManager) CreateVM() error {
-	b, err := xml.Marshal(defaultDomain())
+// GetRunningVMs asks libvirt for current domains and returns them.
+func (v *VMManager) GetRunningVMs() ([]VM, error) {
+	net, err := v.libvirt.NetworkLookupByName("default")
 	if err != nil {
-		fmt.Println("err1", err)
+		return nil, err
+	}
+
+	dms, err := v.libvirt.Domains()
+	if err != nil {
+		return nil, err
+	}
+
+	var runningVMs []VM
+	for i := range dms {
+		if dms[i].ID != -1 {
+			rXML, err := v.libvirt.DomainGetXMLDesc(dms[i], 0)
+			if err != nil {
+				return nil, err
+			}
+
+			domcfg := &libvirtxml.Domain{}
+			if err := domcfg.Unmarshal(rXML); err != nil {
+				return nil, err
+			}
+
+			var macAddr string
+			for _, val := range domcfg.Devices.Interfaces {
+				if val.Source.Network.Network == "default" {
+					macAddr = val.MAC.Address
+				}
+			}
+			if macAddr == "" {
+				macAddr = "pending"
+			}
+
+			ip := "pending"
+			if macAddr != "pending" {
+				m := libvirt.OptString{macAddr}
+				leases, _, err := v.libvirt.NetworkGetDhcpLeases(net, m, 1, 0)
+				if err != nil {
+					return nil, err
+				}
+				for _, val := range leases {
+					if len(val.Mac) == 0 {
+						continue
+					} else if val.Mac[0] == macAddr {
+						ip = val.Ipaddr
+					}
+				}
+			}
+
+			st, maxMem, mem, rNrVirtCPU, cpuTime, err := v.libvirt.DomainGetInfo(dms[i])
+			fmt.Println(st, maxMem, mem, rNrVirtCPU, cpuTime, err)
+
+			state, _, err := v.libvirt.DomainGetState(dms[i], 0)
+			if err != nil {
+				return nil, err
+			}
+
+			vm := VM{
+				ID:    int(dms[i].ID),
+				Name:  dms[i].Name,
+				State: domainState(state),
+				// TODO once I figure out networking they will get their own IP...
+				// For now we have to search for the dhcp lease above and strip it.
+				IP:         ip,
+				MAC:        macAddr,
+				Mem:        int(domcfg.Memory.Value),
+				CurrentMem: int(domcfg.CurrentMemory.Value),
+				VCPUs:      int(domcfg.VCPU.Value),
+				Type:       *domcfg.OS.Type,
+				Devices:    *domcfg.Devices,
+			}
+			runningVMs = append(runningVMs, vm)
+		}
+	}
+
+	return runningVMs, nil
+}
+
+// CreateVM currently handles spinning up the default ubuntu bionic VM
+func (v *VMManager) CreateVM(machineType string, memoryInGB int, vCPUs int) error {
+	id := shortuuid.New()
+
+	pk, err := pubKey(os.Getenv("PUBKEY_LOCATION"))
+	if err != nil {
+		return err
+	}
+
+	if err := prepareHostWithUbuntuDisks(pk, id); err != nil {
+		return err
+	}
+
+	b, err := xml.Marshal(buildDomainXML(id, machineType, memoryInGB, vCPUs))
+	if err != nil {
 		return err
 	}
 
 	domain, err := v.libvirt.DomainCreateXML(string(b), 0)
 	if err != nil {
-		fmt.Println("err2", err)
 		return err
 	}
 	fmt.Printf("domain: %+v", domain)
@@ -67,82 +173,146 @@ func (v *VMManager) CreateVM() error {
 	return err
 }
 
-// GetVMs ...
-func (v *VMManager) GetVMs() ([]libvirt.Domain, error) {
-	dms, err := v.libvirt.Domains()
+func domainState(state int32) string {
+	switch state {
+	case int32(libvirt.DomainNostate):
+		return "unknown"
+	case int32(libvirt.DomainRunning):
+		return "running"
+	case int32(libvirt.DomainBlocked):
+		return "blocked"
+	case int32(libvirt.DomainPaused):
+		return "paused"
+	case int32(libvirt.DomainShutdown):
+		return "shutting down"
+	case int32(libvirt.DomainShutoff):
+		return "off"
+	case int32(libvirt.DomainCrashed):
+		return "crashed"
+	case int32(libvirt.DomainPmsuspended):
+		return "pm suspended"
+	default:
+		return "unknown"
+	}
+}
+
+func pubKey(path string) (ssh.AuthMethod, error) {
+	key, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	return dms, nil
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.PublicKeys(signer), nil
 }
 
-// Creates a domain definition with some defaults
-func defaultDomain() libvirtxml.Domain {
+func prepareHostWithUbuntuDisks(pk ssh.AuthMethod, id string) error {
+	user := "root"
+	remote := "157.245.225.232"
+	port := ":22"
+	config := &ssh.ClientConfig{
+		User:            user,
+		Auth:            []ssh.AuthMethod{pk},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	conn, err := ssh.Dial("tcp", remote+port, config)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	sess, err := conn.NewSession()
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	// Create a single command that is semicolon seperated
+	commands := []string{
+		"cp /var/lib/libvirt/images/ubuntu-bionic.img /var/lib/libvirt/images/ubuntu-bionic-" + id + ".img",
+		"cp /var/lib/libvirt/images/ubuntu-bionic.iso /var/lib/libvirt/images/ubuntu-bionic-" + id + ".iso",
+		"cloud-localds /var/lib/libvirt/images/ubuntu-bionic-" + id + ".iso cloud.txt",
+	}
+	combined := strings.Join(commands, "; ")
+
+	if err := sess.Run(combined); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Currently builds an ubuntu 18.04 bionic beaver image with user defined memroy and cpus.
+func buildDomainXML(id string, machineType string, memoryInGB int, numVCPUs int) libvirtxml.Domain {
 	return libvirtxml.Domain{
 		Type: "kvm",
-		Name: "ubuntu-bionic",
+		Name: "ubuntu-bionic-" + id,
 		OS: &libvirtxml.DomainOS{
 			Type: &libvirtxml.DomainOSType{Type: "hvm"},
 		},
 		Memory: &libvirtxml.DomainMemory{
 			Unit:  "MiB",
-			Value: 1024,
+			Value: gbToMiB(memoryInGB),
 		},
 		VCPU: &libvirtxml.DomainVCPU{
 			Placement: "static",
-			Value:     1,
+			Value:     vCPUCount(numVCPUs),
 		},
-		CPU: &libvirtxml.DomainCPU{},
 		Devices: &libvirtxml.DomainDeviceList{
-			// Emulator: "/usr/bin/qemu-system-x86_64",
-			// Graphics: []libvirtxml.DomainGraphic{{
-			// 	VNC: &libvirtxml.DomainGraphicVNC{},
-			// }},
-			Graphics: []libvirtxml.DomainGraphic{},
 			Interfaces: []libvirtxml.DomainInterface{{
-				Model: &libvirtxml.DomainInterfaceModel{
-					Type: "virtio",
-				},
+				Model: &libvirtxml.DomainInterfaceModel{Type: "virtio"},
 				Source: &libvirtxml.DomainInterfaceSource{
-					Network: &libvirtxml.DomainInterfaceSourceNetwork{
-						Network: "default",
-					},
+					Network: &libvirtxml.DomainInterfaceSourceNetwork{Network: "default"},
 				},
-				// MAC: &libvirtxml.DomainInterfaceMAC{
-				// 	Address: "4a:ee:c7:15:81:d3",
-				// },
-				// IP: []libvirtxml.DomainInterfaceIP{{
-				// 	Address: "192.168.122.10",
-				// 	// Address: "192.168.122.2",
-				// }},
 			}},
 			Disks: []libvirtxml.DomainDisk{{
-				Driver: &libvirtxml.DomainDiskDriver{
-					Type: "raw",
-				},
+				Driver: &libvirtxml.DomainDiskDriver{Type: "raw"},
 				Source: &libvirtxml.DomainDiskSource{
 					File: &libvirtxml.DomainDiskSourceFile{
-						File: "/var/lib/libvirt/images/ubuntu-bionic.img",
+						File: "/var/lib/libvirt/images/ubuntu-bionic-" + id + ".img",
 					},
 				},
-				Target: &libvirtxml.DomainDiskTarget{
-					Dev: "vda",
-					Bus: "virtio",
-				},
+				Target: &libvirtxml.DomainDiskTarget{Dev: "vda", Bus: "virtio"},
 			}, {
-				Driver: &libvirtxml.DomainDiskDriver{
-					Type: "raw",
-				},
+				Driver: &libvirtxml.DomainDiskDriver{Type: "raw"},
 				Source: &libvirtxml.DomainDiskSource{
 					File: &libvirtxml.DomainDiskSourceFile{
-						File: "/var/lib/libvirt/images/ubuntu-bionic.iso",
+						File: "/var/lib/libvirt/images/ubuntu-bionic-" + id + ".iso",
 					},
 				},
-				Target: &libvirtxml.DomainDiskTarget{
-					Dev: "hdc",
-					Bus: "ide",
-				},
+				Target: &libvirtxml.DomainDiskTarget{Dev: "hdc", Bus: "ide"},
 			}},
 		},
+	}
+}
+
+func gbToMiB(gb int) uint {
+	switch gb {
+	case 1:
+		return 1024
+	case 2:
+		return 2048
+	case 4:
+		return 4096
+	case 8:
+		return 8192
+	default:
+		return 2048
+	}
+}
+
+func vCPUCount(numVCPUs int) uint {
+	switch numVCPUs {
+	case 1:
+		return 1
+	case 2:
+		return 2
+	case 4:
+		return 4
+	default:
+		return 1
 	}
 }
--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,9 +10,8 @@ func (a *App) ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
 
-// TODO: implement
 func (a *App) getVMs(c *gin.Context) {
-	domains, err := a.manager.GetVMs()
+	domains, err := a.manager.GetRunningVMs()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -21,9 +19,14 @@ func (a *App) getVMs(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"data": gin.H{"vms": domains}})
 }
 
-// CreateVMReq ...
+// CreateVMReq defines the shape of the JSON request needed from the front end to create a VM.
 type CreateVMReq struct {
+	// MachineType currently is a placeholder until we have more images than ubuntu-18.04
 	MachineType string `json:"machineType" binding:"required"`
+	// Memory in this context is an int representing GB
+	Memory int `json:"memory" binding:"required"`
+	// VCPUs refers to the requested number of vCPUs
+	VCPUs int `json:"vcpus" binding:"required"`
 }
 
 func (a *App) createVM(c *gin.Context) {
@@ -33,10 +36,9 @@ func (a *App) createVM(c *gin.Context) {
 		return
 	}
 
-	// TODO:
-	// - add metadata to database
-	if err := a.manager.CreateVM(); err != nil {
-		fmt.Println(err.Error())
+	// TODO: add metadata to database
+	err := a.manager.CreateVM(vmReq.MachineType, vmReq.Memory, vmReq.VCPUs)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
